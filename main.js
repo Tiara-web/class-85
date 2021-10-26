4431e37176d0031788e7bc9409c8f522a0a5e1e2
Tiara-web <80862602+Tiara-web@users.noreply.github.com>
2021-10-26 19:59:17 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,115 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+rover_width=100;
+rover_heigth=90;
+background_img= "mars.jpg";
+rover_image="rover.png";
+rover_x=10;
+rover_y=10;
+
+function add()
+{
+    background_imgTag= new Image();
+    background_imgTag.onload= uploadBackground;
+    background_imgTag.src= background_img;
+
+    rover_imageTag= new Image();
+    rover_imageTag.onload= uploadrover;
+    rover_imageTag.src= rover_image;    
+}
+
+function uploadBackground()
+{
+    ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);
+
+}
+
+function uploadrover()
+{
+    ctx.drawImage(rover_imageTag,rover_x,rover_y,rover_width,rover_heigth);
+}
+
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e)
+{
+    keyPressed= e.keyCode;
+    console.log(keyPressed);
+    if(keyPressed=="38")
+    {
+        up();
+        console.log("up");
+
+    }
+
+    if(keyPressed=="40")
+    {
+        down();
+        console.log("down");
+        
+    }
+
+    if(keyPressed=="37")
+    {
+        left();
+        console.log("left");
+        
+    }
+
+    if(keyPressed=="39")
+    {
+        right();
+        console.log("right");
+        
+    }
+}
+
+function  up()
+{
+    if (rover_y >=0)
+    {
+        rover_y=rover_y-10;
+        console.log("up arrow pressed x="+rover_x+",y="+rover_y);
+        uploadBackground();
+        uploadrover();
+
+    }
+}
+
+function down()
+
+{
+    if (rover_y <=500)
+    {
+        rover_y=rover_y+10;
+        console.log("down arrow pressed x="+rover_x+",y="+rover_y);
+        uploadBackground();
+        uploadrover();
+
+    }
+}
+
+function left()
+
+{
+    if (rover_x >=0)
+    {
+        rover_x=rover_x-10;
+        console.log("left arrow pressed x="+rover_x+",y="+rover_y);
+        uploadBackground();
+        uploadrover();
+
+    }
+}
+
+function right()
+
+{
+    if (rover_x <=700)
+    {
+        rover_x=rover_x+10;
+        console.log("right arrow pressed x="+rover_x+",y="+rover_y);
+        uploadBackground();
+        uploadrover();
+
+    }
+}
